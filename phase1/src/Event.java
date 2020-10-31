ad66f1f3f7e44f721014b1b47c78c2c76e7b8734
Qi Zheng <wqi.zheng@mail.utoronto.ca>
2020-11-01 02:12:42 +0300

    First version of Event class

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Event {
+    String title;
+    String location;
+    String datetime; // "In the format DD.MM.YYYY - HH.MM"
+    List<Attendee> attendees;
+
+
+    public Event(String title, String location, String datetime) {
+        this.title = title;
+        this.location = location;
+        this.datetime = datetime;
+        this.attendees = new ArrayList<Attendee>();
+    }
+    public List<Attendee> getAttendees() {
+        return this.attendees;
+    }
+
+    public void setAttendees(List<Attendee> attendees) {
+        this.attendees = attendees;
+    }
+
+    public Object[] getEventInfo(){
+        return new Object[]{this.title, this.location, this.datetime, this.attendees};
+    }
+
 }
