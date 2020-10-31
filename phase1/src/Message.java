f2e8d69f909036943445d4e35c68cc99a8c5cdcf
werlezac <zach.werle@mail.utoronto.ca>
2020-10-31 03:40:42 -0700

    Zach - Updated Message Entity. Please take a look when you have time.

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -1,2 +1,25 @@
+import java.time.*;
+
 public class Message {
+    private final String content;
+    private final String sender;
+    private final LocalDateTime time_sent;
+
+    public Message(String content, String sender){
+        this.content = content;
+        this.sender = sender;
+        time_sent = LocalDateTime.now(ZoneId.of("America/Toronto"));
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public String getSender(){
+        return sender;
+    }
+
+    public LocalDateTime getTime_sent() {
+        return time_sent;
+    }
 }
