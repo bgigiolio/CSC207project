414ce33996efc9137a53cb14a67926c53582a6e6
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2020-10-31 20:22:25 -0400

    Created baseline for class!

--- phase1/src/Schedule.java
+++ phase1/src/Schedule.java
@@ -1,2 +1,43 @@
+import java.util.*;
 public class Schedule {
+    private final HashMap<Object, Event> scheduleMap = new HashMap<>();
+    private int endHour;
+    private int startHour;
+
+    public Schedule(int startHour, int endHour){
+        this.endHour = endHour;
+        this.startHour = startHour;
+    }
+    public boolean addEvent(Event e){
+        String eventTime = e.getDatetime();
+        String eventHour = eventTime.substring(13, 15);
+        int hour = Integer.parseInt(eventHour);
+
+        if ((scheduleMap.containsKey(eventTime) && e.getLocation().equals(scheduleMap.get(eventTime).location))
+                || hour >= endHour || hour < startHour) {
+            return false;
+        }
+
+        scheduleMap.put(eventTime, e);
+        return true;
+    }
+
+    public boolean addEvent(String title, String location, String datetime){
+        Event e = new Event(title, location, datetime);
+        return this.addEvent(e);
+    }
+
+    public boolean removeEvent(Event e){
+        if (scheduleMap.containsKey(e.getDatetime())) {
+            scheduleMap.remove(e.getDatetime());
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void editHours(int startHour, int endHour){
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
 }
