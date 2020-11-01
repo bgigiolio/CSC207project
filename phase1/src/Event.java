d0552d22995635cbe6c54afef10b96b5147cebc6
Qi Zheng <wqi.zheng@mail.utoronto.ca>
2020-11-01 03:03:20 +0300

    Second version of Event class: -getters and setters for individual variables are added

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -15,8 +15,32 @@ public class Event {
         this.datetime = datetime;
         this.attendees = new ArrayList<Attendee>();
     }
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public String getDatetime() {
+        return this.datetime;
+    }
+
     public List<Attendee> getAttendees() {
         return this.attendees;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public void setDatetime(String datetime){
+        this.datetime = datetime;
     }
 
     public void setAttendees(List<Attendee> attendees) {
