84c1c1aae7388c72bf5c85bdb478defd3673c779
Morgan Chang <morgan.chang@mail.utoronto.ca>
2020-11-06 06:40:50 +0800

    First version of Organizer

--- phase1/src/Organizer.java
+++ phase1/src/Organizer.java
@@ -1,2 +1,27 @@
-public class Organizer {
+import java.util.List;
+
+public class Organizer extends Attendee {
+
+    String username;
+    String password;
+    String userid;
+    List<Attendee> friendlist; // list of username?
+    List<Message> messages;
+    List<Event> registration;
+
+    public Organizer(String username, String password) {
+        super(username, password);
+    }
+
+    // public boolean createRoom;
+    // DO LATER: not sure about how to create room since we're only having one room ?
+
+    // public Speaker createSpeakerAccount (String username, String password) {
+        // return Speaker(username, password) } // implement Speaker first
+
+    public void assignSpeaker (String speakerUsername, Talk talk){
+        talk.setSpeaker(speakerUsername); // implement Speaker first, change later
+    }
+
 }
+
