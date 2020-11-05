e7dc83ee4c565459e1b398a05460211f9fe4038b
Utkarsh <utkarsh.mali@mail.utoronto.ca>
2020-11-05 12:14:29 -0500

    Completed MessageController, MessageCreator

--- phase1/src/MessageController.java
+++ phase1/src/MessageController.java
@@ -1,2 +1,6 @@
 public class MessageController {
+    // The bridge between Message Creator and Attendee's Message List
+    public MessageController(Attendee person, Message inputMessage) {
+        person.messages.add(inputMessage);
+    }
 }
--- phase1/src/MessageCreator.java
+++ phase1/src/MessageCreator.java
@@ -1,2 +1,18 @@
 public class MessageCreator {
+    protected Attendee sender;
+    protected Attendee reciever;
+    protected Message message;
+
+    public MessageCreator(String toBeMessage, Attendee reciever, Attendee sender){
+        this.reciever = reciever;
+        this.sender = sender;
+        message = new Message(toBeMessage, sender.getUserid());
+        deliver();
+    }
+
+    private void deliver(){
+        new MessageController(this.reciever, this.message);
+    }
+
+
 }
