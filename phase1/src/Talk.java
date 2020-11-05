00827d0145a09c0c2d464d43e2b9550be335f0c7
Utkarsh <utkarsh.mali@mail.utoronto.ca>
2020-11-05 11:18:40 -0500

    Added Talk Class Added Handles for MessageController/Creator

--- phase1/src/Talk.java
+++ phase1/src/Talk.java
@@ -1,2 +1,15 @@
-public class Talk {
+public class Talk extends Event{
+    protected String speaker;
+    public Talk(String title, String location, String datetime, String speaker_name) {
+        super(title, location, datetime);
+        this.speaker = speaker_name;
+    }
+
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
 }
