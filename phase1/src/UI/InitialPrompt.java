39b73e485e067a231243128291061d56ab86f788
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2020-11-07 22:24:01 -0500

    changed public to private (oops)

--- phase1/src/UI/InitialPrompt.java
+++ phase1/src/UI/InitialPrompt.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 //These UI classes are just thrown together to make running the program a bit easier.
 // PLEASE dont be afraid to delete these and change them a bunch!!!
 public class InitialPrompt {
-    public Object Menu;
+    private Object Menu;
     public void startProgram() {
         boolean answered = false;
         Scanner uname = new Scanner(System.in);
