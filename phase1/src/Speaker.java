56c6b7adec84abb09004fbe28113c1b85216cfed
Utkarsh <utkarsh.mali@mail.utoronto.ca>
2020-11-08 10:47:09 -0500

    Initial Speaker Class

--- phase1/src/Speaker.java
+++ phase1/src/Speaker.java
@@ -1,2 +1,19 @@
-public class Speaker {
+import java.util.List;
+
+// Basic Implimentation of speaker, please edit.
+
+public class Speaker extends Attendee {
+
+    String username;
+    String password;
+    String userid;
+    List<Attendee> friendlist; // list of username?
+    List<Message> messages;
+    List<Event> registration;
+    String role;
+
+    public Speaker(String username, String password) {
+        super(username, password);
+        this.role = "speaker";
+    }
 }
