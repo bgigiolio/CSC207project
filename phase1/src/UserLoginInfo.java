319aa2a39b7179af3653ef2b791aa0de5839a8d2
Qi Zheng <wqi.zheng@mail.utoronto.ca>
2020-11-09 14:12:22 +0300

    Implemented the gateway class UserLoginInfo
    In event class:
    - Changed the type of datetime variable to LocalDateTime
    - Modified the method setAttendees; the method will change the whole list. Added two overloaded methods addAttendees and removeAttendees, which will either add/remove an individual or individuals to/from the existing list.

---
+++ phase1/src/UserLoginInfo.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+// GATEWAY
+public class UserLoginInfo implements Serializable{
+
+    private HashMap<String, String> LoginInfo;
+
+    public UserLoginInfo(){
+        HashMap<String, String> LoginInfo = new HashMap<>();
+    }
+
+    public HashMap<String, String> getLoginInfo() {
+        return LoginInfo;
+    }
+
+    public void addUserInfo(String username, String password){
+        if(!LoginInfo.containsKey(username)){ LoginInfo.put(username, password); }
+    }
+
+    public void removeUserInfo(String username){
+        LoginInfo.remove(username);
+    }
+
+    public HashMap<String, String> getFileUserLoginInfo(String filePath) throws IOException {
+        try{
+            InputStream file = new FileInputStream(filePath);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            LoginInfo = (HashMap<String, String>) input.readObject();
+            input.close();
+        } catch (FileNotFoundException | ClassNotFoundException e){
+            e.printStackTrace();
+            System.out.println("Existing version is printed.");
+        }
+        return LoginInfo;
+    }
+
+    public void setFileUserLoginInfo(String filePath) throws IOException {
+        OutputStream file = new FileOutputStream(filePath);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutput output = new ObjectOutputStream(buffer);
+
+        output.writeObject(this.LoginInfo);
+        output.close();
+    }
+}
