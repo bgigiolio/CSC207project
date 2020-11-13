9b044fbd48c56897d8eb4f4b4763eb2d03837b3c
Qi Zheng <wqi.zheng@mail.utoronto.ca>
2020-11-13 03:37:29 +0300

    Minor changes and added setLoginInfo method to UserLoginInfo. Implemented MessageGateway Implemented FriendListGateway

---
+++ phase1/src/FriendListGateway.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class FriendListGateway implements Serializable{
+
+    private HashMap<String, ArrayList<String>> friendlistDatabase;
+
+    public FriendListGateway(HashMap<String, ArrayList<String>> friendlistDatabase){
+        this.friendlistDatabase = friendlistDatabase;
+    }
+
+    public FriendListGateway(){
+        friendlistDatabase = new HashMap<>();
+    }
+
+    public HashMap<String, ArrayList<String>> getFriendlistDatabase(){
+        return this.friendlistDatabase;
+    }
+
+    public void setFriendlistDatabase(HashMap<String, ArrayList<String>> friendlistDatabase){
+        this.friendlistDatabase = friendlistDatabase;
+    }
+
+
+    public HashMap<String, ArrayList<String>> getFriendlistFile(String filePath) throws IOException {
+        try{
+            InputStream file = new FileInputStream(filePath);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInputStream input = new ObjectInputStream(buffer);
+
+            this.friendlistDatabase = (HashMap<String, ArrayList<String>>) input.readObject();
+            input.close();
+        }
+        catch (FileNotFoundException|ClassNotFoundException e){
+            e.printStackTrace();
+            System.out.println("Existing friendlist database is returned");
+        }
+        return this.friendlistDatabase;
+    }
+
+    public void setFriendlistDatabase(String filePath) throws IOException{
+        try {
+            OutputStream file = new FileOutputStream(filePath);
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutputStream output = new ObjectOutputStream(buffer);
+
+            output.writeObject(this.friendlistDatabase);
+            output.close();
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ phase1/src/MessageGateway.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class MessageGateway implements Serializable{
+    private HashMap<String, ArrayList<ArrayList<Message>>> messageDatabase;
+
+    public MessageGateway(HashMap<String, ArrayList<ArrayList<Message>>> messagedatabase){
+        this.messageDatabase = messagedatabase;
+    }
+
+    public MessageGateway(){
+        this.messageDatabase = new HashMap<>();
+    }
+
+    public HashMap<String, ArrayList<ArrayList<Message>>> getMessageDatabase(){
+        return this.messageDatabase;
+    }
+    public void setMessageDatabase(HashMap<String, ArrayList<ArrayList<Message>>> messageDatabase){
+        this.messageDatabase = messageDatabase;
+    }
+
+
+    public HashMap<String, ArrayList<ArrayList<Message>>> getMessageFile(String filePath) throws IOException{
+        try{
+            InputStream file = new FileInputStream(filePath);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            this.messageDatabase = (HashMap<String, ArrayList<ArrayList<Message>>>) input.readObject();
+            input.close();
+        }
+        catch (FileNotFoundException|ClassNotFoundException e){
+            e.printStackTrace();
+            System.out.println("Existing version of messagedatabase is returned");
+        }
+        return messageDatabase;
+    }
+
+    public void setMessageDatabase(String filePath) throws IOException{
+        try{
+            OutputStream file = new FileOutputStream(filePath);
+            OutputStream buffer = new BufferedOutputStream(file);
+            ObjectOutput output = new ObjectOutputStream(buffer);
+
+            output.writeObject(this.messageDatabase);
+            output.close();
+
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
