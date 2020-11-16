89ec59fb003a645c6196895ee874cc00ac9e0e31
Morgan Chang <morgan.chang@mail.utoronto.ca>
2020-11-15 22:48:00 -0500

    update event status changer class

--- phase1/src/Controllers/EventStatusChanger.java
+++ phase1/src/Controllers/EventStatusChanger.java
@@ -1,10 +1,37 @@
 package Controllers;
 
+import Entities.Event;
+import Gateways.EventGateway;
 import UseCases.*;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EventStatusChanger {
 
-    public boolean joinEvent(String eventTitle) {
+    private EventGateway eventGateway = new EventGateway();
+    private EventStatus eventStatus;
+
+    public EventStatusChanger(EventStatus eventStatus) {
+        this.eventStatus = eventStatus;
+    }
+
+    public boolean joinEvent(String eventTitle) throws ClassNotFoundException {
+        try {
+            HashMap<LocalDate, ArrayList<Event>> existingEvents = this.eventGateway.getEvents();
+            for (ArrayList<Event> value : existingEvents.values()) {
+                for (Event searchEvent : value) {
+                    if (searchEvent.getTitle().equals(eventTitle)) {
+                        this.eventStatus.signUp(searchEvent);
+                        return true;
+                    }
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } return false;
 
     }
 }
