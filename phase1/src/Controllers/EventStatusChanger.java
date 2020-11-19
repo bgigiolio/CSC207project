490edb0ffaf0f000b7a7254a62ab30419a6a8489
Morgan Chang <morgan.chang@mail.utoronto.ca>
2020-11-19 02:54:31 -0500

    Added javadoc for EventStatusChanger

--- phase1/src/Controllers/EventStatusChanger.java
+++ phase1/src/Controllers/EventStatusChanger.java
@@ -1,23 +1,29 @@
 package Controllers;
 
-import Entities.Event;
-import Gateways.EventGateway;
-import Gateways.EventStatusGateway;
-import UseCases.*;
-
+import Gateways.*;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
 
+/**
+ * <h1>Event Status Changer</h1>
+ * Controls the gateway responsible for storing EventStatus object.
+ *
+ * @author Morgan Chang
+ */
 public class EventStatusChanger {
 
-    private EventStatusGateway eventStatusGateway;
+    /**
+     * The gateway to be controlled.
+     */
+    private EventStatusGateway eventStatusGateway = new EventStatusGateway();
 
-    public EventStatusChanger() {
-        this.eventStatusGateway = new EventStatusGateway();
-    }
-
+    /**
+     * Read from a file, EventStatusData.ser, about existing users' event registrations,
+     * register the user of username for the event of eventTitle
+     * iff the user has not signed up for the event yet,
+     * and update the file.
+     *
+     * @return true if username has successfully signed up for eventTitle.
+     */
     public boolean signUpForEvent(String username, String eventTitle) throws IOException {
         String db = "phase1/src/DB/EventStatusData.ser";
         eventStatusGateway.setEventStatus(eventStatusGateway.loadFromFile(db));
@@ -26,6 +32,14 @@ public class EventStatusChanger {
         return returnVal;
     }
 
+    /**
+     * Read from a file, EventStatusData.ser, about existing users' event registrations,
+     * cancel the enrollment of the user of username from the event of eventTitle
+     * iff the user had signed up for the event,
+     * and update the file.
+     *
+     * @return true iff the user did not register for the event, and has successfully signed up.
+     */
     public boolean cancelEvent(String username, String eventTitle) throws IOException {
         String db = "phase1/src/DB/EventStatusData.ser";
         eventStatusGateway.setEventStatus(eventStatusGateway.loadFromFile(db));
@@ -34,32 +48,4 @@ public class EventStatusChanger {
         return returnVal;
     }
 
-//    private EventGateway eventGateway = new EventGateway();
-//    private EventStatus eventStatus;
-//
-//    public EventStatusChanger(EventStatus eventStatus) {
-//        this.eventStatus = eventStatus;
-//    }
-//
-//    // joinEvent takes a String (eventTitle) as the input.
-//    // This is how it works:
-//    // The presenter asks user to input the event title that user want to sign up for
-//    // --> presenter calls EventStatusChanger.joinEvent(eventTitle)
-//    // --> save signed up event into EventStatus class
-//    // problem: where do we instantiate EventStatusChanger and EventStatus in presenter?
-//    public boolean joinEvent(String eventTitle) throws ClassNotFoundException {
-//        try {
-//            HashMap<LocalDate, ArrayList<Event>> existingEvents = this.eventGateway.getEvents();
-//            for (ArrayList<Event> value : existingEvents.values()) {
-//                for (Event searchEvent : value) {
-//                    if (searchEvent.getTitle().equals(eventTitle)) {
-//                        this.eventStatus.signUp(searchEvent);
-//                        return true;
-//                    }
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            ex.printStackTrace();
-//        } return false;
-//    }
 }
