b898fa256cc7c92ac4f0ce4ee5e3de1137f75461
Morgan Chang <morgan.chang@mail.utoronto.ca>
2020-11-19 03:16:56 -0500

    Added javadoc for EventStatusGateway

--- phase1/src/Gateways/EventStatusGateway.java
+++ phase1/src/Gateways/EventStatusGateway.java
@@ -1,27 +1,40 @@
 package Gateways;
 
-import Entities.Event;
 import UseCases.EventStatus;
-import UseCases.LoginUserManager;
-
 import java.io.*;
 
+/**
+ * <h1>Event Status Gateway</h1>
+ * Loads file about the system responsible for storing a hashmap that maps a user's username
+ * to a List of event title that user had signed up for, or save it to file.
+ *
+ * @author Morgan Chang
+ */
 public class EventStatusGateway {
 
-    private EventStatus eventStatus;
-
-    public EventStatusGateway() {
-        this.eventStatus = new EventStatus();
-    }
+    /**
+     * The system to be load or save to a file.
+     */
+    private EventStatus eventStatus = new EventStatus();
 
     public EventStatus getEventStatus() {
         return eventStatus;
     }
 
+    /**
+     * Set EventStatus.
+     *
+     * @param eventStatus the EventStatus object that stores the data about event registrations of users
+     */
     public void setEventStatus(EventStatus eventStatus) {
         this.eventStatus = eventStatus;
     }
 
+    /**
+     * Load filepath to controller.
+     *
+     * @return eventStatus the EventStatus object with existing data about event registrations of users
+     */
     public EventStatus loadFromFile(String filePath) throws IOException {
         try{
             InputStream file = new FileInputStream(filePath);
@@ -36,6 +49,11 @@ public class EventStatusGateway {
         return eventStatus;
     }
 
+    /**
+     * Save eventStatus to filepath.
+     *
+     * @param filePath the name of the file
+     */
     public void saveToFile(String filePath) throws IOException {
         try {
             OutputStream file = new FileOutputStream(filePath);
