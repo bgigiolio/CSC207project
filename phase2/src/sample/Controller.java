46d0b006f53b92c2c2e7d73f2e975fdd2d4d11ec
Qi Zheng <wqi.zheng@mail.utoronto.ca>
2020-12-04 06:00:15 +0300

    Implemented handleNewUserButton and added NewUserLoginScene.fxml

--- phase2/src/sample/Controller.java
+++ phase2/src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class Controller {
+    @FXML
+    Button newUserButton;
+    @FXML
+    Button retUserButton;
+    @FXML
+    Button loginButton;
+    @FXML
+    Button backButton;
+
+    //reminder: initializer
+    public void handleNewUserButton(ActionEvent event) throws IOException {
+ //or use static method
+        Parent newUserRoot = FXMLLoader.load(getClass().getResource("NewUserLoginScene.fxml"));
+        Stage primaryStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene newUserLoginScene = new Scene(newUserRoot);
+        primaryStage.setScene(newUserLoginScene);
+        primaryStage.show();
+    }
+    public void handleRetUserButton(){
+
+    }
 }
--- phase2/src/sample/Main.java
+++ phase2/src/sample/Main.java
@@ -14,6 +14,8 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 
+import java.io.IOException;
+
 public class Main extends Application {
 //    Button button;
     Button newButton;
@@ -26,82 +28,87 @@ public class Main extends Application {
     Scene sceneNewLogin;
     Scene sceneReturningLogin;
 
+    Stage primaryStage;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//
-//        button = new Button();
-//        button.setText("New user");
-//        StackPane layout = new StackPane();
-//        layout.getChildren().add(button);
+    public void start(Stage primaryStage) throws Exception {
 
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene introScene = new Scene(root, 300, 275);
         primaryStage.setTitle("Building Manager");
+        primaryStage.setScene(introScene);
         primaryStage.show();
-
-
-        //Opening window
-        newButton = new Button("New User");
-        returningButton = new Button("Returning User");
-//      New user button
-        newButton.setOnAction(event -> primaryStage.setScene(sceneNewLogin));
-//      Returning user button
-        returningButton.setOnAction(event -> primaryStage.setScene(sceneReturningLogin));
-
-
-
-        VBox vBox = new VBox();
-        vBox.setSpacing(10);
-        ObservableList<Node> buttonList = vBox.getChildren();
-        buttonList.addAll(newButton, returningButton);
-        vBox.setAlignment(Pos.CENTER);
-        sceneIntro = new Scene(vBox, 300, 250);
-        primaryStage.setScene(sceneIntro);
-        primaryStage.show();
-
-
-
-        //New login window
-        VBox newLogVBox = new VBox();
-        sceneNewLogin = new Scene(newLogVBox, 300, 250);
-        primaryStage.show();
-        TextField newUsername = new TextField("New Username");
-        TextField newPassword = new TextField("New Password");
-        newUsername.setMaxWidth(150);
-        newPassword.setMaxWidth(150);
-        ObservableList<Node> inputs = newLogVBox.getChildren();
-        newLoginButton = new Button("Login");
-        inputs.addAll(newUsername, newPassword,newLoginButton);
-        newLogVBox.setAlignment(Pos.CENTER);
-
-
-        //Returning login window
-        retLoginButton = new Button("Login");
-        GridPane retLogGrid = new GridPane();
-        retLogGrid.setAlignment(Pos.CENTER);
-        retLogGrid.setHgap(10);
-        retLogGrid.setVgap(10);
-        Label retUsername = new Label("Username");
-        Label retPassword = new Label("Password");
-        retLogGrid.add(retUsername, 0,1);
-        retLogGrid.add(retPassword, 0,2);
-        TextField usernameBox = new TextField();
-        TextField passwordBox = new TextField();
-        usernameBox.setMaxWidth(150);
-        passwordBox.setMaxWidth(150);
-        retLogGrid.add(usernameBox,1,1);
-        retLogGrid.add(passwordBox,1,2);
-        retLogGrid.add(retLoginButton,1,3);
-        sceneReturningLogin = new Scene(retLogGrid, 300, 250);
-
-
-
-
-
-
-
-
 
     }
+
+
+
+
+////
+////        button = new Button();
+////        button.setText("New user");
+////        StackPane layout = new StackPane();
+////        layout.getChildren().add(button);
+//
+//        primaryStage.setTitle("ConfApp");
+//        primaryStage.show();
+//
+//
+//        //Opening window
+//        newButton = new Button("New User");
+//        returningButton = new Button("Returning User");
+////      New user button
+//        newButton.setOnAction(event -> primaryStage.setScene(sceneNewLogin));
+////      Returning user button
+//        returningButton.setOnAction(event -> primaryStage.setScene(sceneReturningLogin));
+//
+//
+//
+//        VBox vBox = new VBox();
+//        vBox.setSpacing(10);
+//        ObservableList<Node> buttonList = vBox.getChildren();
+//        buttonList.addAll(newButton, returningButton);
+//        vBox.setAlignment(Pos.CENTER);
+//        sceneIntro = new Scene(vBox, 300, 250);
+//        primaryStage.setScene(sceneIntro);
+//        primaryStage.show();
+//
+//
+//
+//        //New login window
+//        VBox newLogVBox = new VBox();
+//        sceneNewLogin = new Scene(newLogVBox, 300, 250);
+//        primaryStage.show();
+//        TextField newUsername = new TextField("New Username");
+//        TextField newPassword = new TextField("New Password");
+//        newUsername.setMaxWidth(150);
+//        newPassword.setMaxWidth(150);
+//        ObservableList<Node> inputs = newLogVBox.getChildren();
+//        newLoginButton = new Button("Login");
+//        inputs.addAll(newUsername, newPassword,newLoginButton);
+//        newLogVBox.setAlignment(Pos.CENTER);
+//
+//
+//        //Returning login window
+//        retLoginButton = new Button("Login");
+//        GridPane retLogGrid = new GridPane();
+//        retLogGrid.setAlignment(Pos.CENTER);
+//        retLogGrid.setHgap(10);
+//        retLogGrid.setVgap(10);
+//        Label retUsername = new Label("Username");
+//        Label retPassword = new Label("Password");
+//        retLogGrid.add(retUsername, 0,1);
+//        retLogGrid.add(retPassword, 0,2);
+//        TextField usernameBox = new TextField();
+//        TextField passwordBox = new TextField();
+//        usernameBox.setMaxWidth(150);
+//        passwordBox.setMaxWidth(150);
+//        retLogGrid.add(usernameBox,1,1);
+//        retLogGrid.add(passwordBox,1,2);
+//        retLogGrid.add(retLoginButton,1,3);
+//        sceneReturningLogin = new Scene(retLogGrid, 300, 250);
+
+//    }
 
 
     public static void main(String[] args) {
