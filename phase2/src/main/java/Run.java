5bd7b6950606306c87815851da1313f10f9fc134
Blake Gigiolio <blake.gigiolio@mail.utoronto.ca>
2020-12-06 10:30:33 -0700

    Added implementation for the new main.java package

--- phase2/src/main/java/Run.java
+++ phase2/src/main/java/Run.java
@@ -9,14 +9,13 @@ import java.io.IOException;
 public class Run {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        //If we serialized the building class, we will want to load the serialized building here
         ImportExportEventController eventGateway = new ImportExportEventController();
 
         ProgramMain sys = new ProgramMain(eventGateway.importEvents());
 
         sys.start();
 
-        // Main gui = new Main();
-        // Main.launch();
+//         Main gui = new Main();
+//         gui.launch();
     }
 }
