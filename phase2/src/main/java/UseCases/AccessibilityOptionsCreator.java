a16b27ee131bd3d006bcd68bb440e0e47ea5d29a
Yuteng Gao <bobby.gaoyuteng@gmail.com>
2020-12-09 20:05:49 +0800

    add AccessibilityOptionsCreator

---
+++ phase2/src/main/java/UseCases/AccessibilityOptionsCreator.java
@@ -0,0 +1,26 @@
+package phase2.src.main.java.UseCases;
+
+import java.time.LocalDateTime;
+
+public class AccessibilityOptionsCreator {
+
+    protected AccessibilityOptions accessibilityOption;
+
+    public AccessibilityOptionsCreator(String request, String sender){
+        if (request.equals("food") || request.equals("transportation") || request.equals("vision")){
+            accessibilityOption = new AccessibilityOptions(request, sender);
+        }
+        else {
+            System.err.println("This is not included in our accessibility options.");
+        }
+    }
+
+    public LocalDateTime getTimeSent(){
+        return accessibilityOption.getTime_sent();
+    }
+
+    public AccessibilityOptions getRequest() {
+        return accessibilityOption;
+    }
+
+}
