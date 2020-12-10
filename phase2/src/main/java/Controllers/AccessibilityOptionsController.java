c4b7325c9fb542f45e643f34e873ea8727e71d11
Yuteng Gao <bobby.gaoyuteng@gmail.com>
2020-12-10 21:14:51 +0800

    improve the AccessibilityOptionsController

--- phase2/src/main/java/Controllers/AccessibilityOptionsController.java
+++ phase2/src/main/java/Controllers/AccessibilityOptionsController.java
@@ -5,25 +5,54 @@ import src.main.java.Gateways.AccessibilityOptionsGateway;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class AccessibilityOptionsController {
+/**
+ * This AccessibilityOptions controller is responsible for sending and deleting request by users, and
+ * managing these requests by organizers.
+ * @author Yuteng Gao
+ * @version phase2
+ */
 
+public class AccessibilityOptionsController {
+    /**
+     * The username of the sender
+     */
     private final String sender;
+    /**
+     * the request of the sender
+     */
     private final String request;
+    /**
+     * the request list as an AccessibilityOptionsGateway object
+     */
     private final AccessibilityOptionsGateway requestList;
+    /**
+     * creator of the new request
+     */
     private final main.java.UseCases.AccessibilityOptionsCreator requestCreator;
 
+    /**
+     * the constructor of the Accessibility Options controller
+     * @param sender the username of the sender
+     * @param request the accessibility request of the sender
+     */
     public AccessibilityOptionsController(String sender, String request){
         this.sender = sender;
         this.request = request;
         this.requestList = new AccessibilityOptionsGateway();
         this.requestCreator = new main.java.UseCases.AccessibilityOptionsCreator(this.request, this.sender);
-
     }
+
+    /**
+     * send the request from the sender to request list
+     */
     public void sendRequest(){
         this.requestList.addRequest(this.sender, this.requestCreator.getRequest());
 
     }
 
+    /**
+     * delete the request from the request list
+     */
     public void deleteRequest(){
         this.requestList.removeRequest(this.sender, this.requestCreator.getRequest());
     }
