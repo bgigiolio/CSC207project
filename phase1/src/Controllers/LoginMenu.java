60d3a22431e490845aca4484cde51a8d9f087286
bgigiolio <80730396+bgigiolio@users.noreply.github.com>
2021-03-15 22:51:14 -0400

    Delete phase1 directory

--- phase1/src/Controllers/LoginMenu.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
-import java.io.IOException;
-
-public interface LoginMenu {
-    String usernamePrompt();
-    String passwordPrompt();
-    boolean logReg(String username, String password, String role) throws IOException;
-}
--- phase1/test/LoginUserManagerTest.java
+++
@@ -1,5 +0,0 @@
-//public class LoginUserManagerTest {
-//    public void testLoginUserManager(){
-//
-//    }
-//}
